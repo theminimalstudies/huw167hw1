da94ccea72ebdfd48865fa2417c6d6ad1ad37da1
theminimalstudies <43444997+theminimalstudies@users.noreply.github.com>
2019-06-05 17:03:33 -0400

    Add files via upload

---
+++ homework1.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+var d= new date (12);
+var time= d.gethours (8);
+
+if (time < 12) {
+	document.write ("<b>Good morning!!</b>");
+	
+}
+
+if (time > 12) {
+	document.write ("<b>Good Afternoon!! </b>")
+}
+
+if )time == 12){
+	document.write (<b>Time to go To work!</b> )
+}
